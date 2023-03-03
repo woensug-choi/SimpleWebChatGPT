d4ec367b444bbd9d85724c6270806f8388093381
Choi Woen-Sug <woensug.choi@gmail.com>
2023-03-03 09:37:45 +0900

    initial commit

--- README.md
+++ README.md
@@ -1,26 +1,172 @@
-# Django Chatbot hosted on Heroku
+# Web-Based ChatGPT Chatbot with Django
 
-Django Chatbot with the background tasks processing and communications via WebSockets.
+- Web-Based ChatGPT Chatbot
+  - Remember the conversation in a session
+  - Uses NanumSquareNeo font (Neat looking for Korean)
 
+Chatbot with the background tasks processing and communications via WebSockets.
 For more details please check my article - [Heroku Chatbot with Celery, WebSockets, and Redis](https://itnext.io/heroku-chatbot-with-celery-websockets-and-redis-340fcd160f06).
+
+Supported by <a href="https://woensug-choi.github.io/" style="text-decoration: none; color: rgb(42, 144, 42); font-weight: bold;">Choi Woen-Sug (Ocean Eng., KMOU)</a>🧑‍🏫
+
+## Installation
+
+- Requirements
+  - Supported system : Debian Linux (e.g. Ubuntu)
+  - Tested system : Ubuntu 22.04
+
+### Install Redis
+
+```bash
+curl -fsSL https://packages.redis.io/gpg | sudo gpg --dearmor -o /usr/share/keyrings/redis-archive-keyring.gpg
+echo "deb [signed-by=/usr/share/keyrings/redis-archive-keyring.gpg] https://packages.redis.io/deb $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/redis.list
+sudo apt update && sudo apt install -y redis
+```
+
+Check redis by running with following command,
+
+```bash
+redis-cli
+```
+You may see redis url and port. To exit, type `exit`.
+
+## Environment Variables
+
+- REDIS URL
+  - default redis url and port is 127.0.0.1:6379
+- DJANGO SECRET KEY
+  - Obtain at https://djecrety.ir/
+  - Replafe `YOUR_DJGANGO_SECRET_KEY` with your key
+- OPENAI MODEL
+  - Name of the model you want to use.
+  - ChatGPT version 3 is `text-davinci-003`
+- OPENAI API KEY
+  - Obtain at https://platform.openai.com/account/api-keys
+  - Replace `YOUR_API_KEY` with your key
+- PROMPT OPTION
+  - It's an option you want to give to add exaplanation to the prompt.
+  - It will be added to the prompt to help better responses.
+  - Example would be `Response in 40 words maximum`. You may add as much as you want.
+
+### Set variables
+
+  ```bash
+  export PROMPT_OPTION="Response in 40 words maximum."
+  export REDIS_URL="redis://127.0.0.1:6379"
+  export DJANGO_SECRET_KEY="YOUR_DJGANGO_SECRET_KEY"
+  export OPENAI_MODEL='text-davinci-003'
+  export OPENAI_API_KEY="YOUR_API_KEY"
+  ```
+
+- If you want to fix it and don't type every time
+  
+  ```bash
+  # Export Redis URL (default redis url and port is 127.0.0.1:6379)
+  echo "export REDIS_URL='redis://127.0.0.1:6379'" >> ~/.bashrc
+
+  # Export Django Secret Key
+  echo 'export DJANGO_SECRET_KEY="YOUR_DJGANGO_SECRET_KEY"' >> ~/.bashrc
+
+  # Export the name of the OpenAI Model
+  echo 'export OPENAI_MODEL="text-davinci-003"' >> ~/.bashrc
+
+  # Export OpenAI ChatGPT API Key
+  echo 'export OPENAI_API_KEY=YOUR_API_KEY' >> ~/.bashrc
+  ```
+
+### Load bashrc for environment variables
+
+```bash
+source ~/.bashrc
+```
+
+## Get source code
+
+```bash
+git clone git@github.com:woensug-choi/SimpleWebChatGPT.git
+```
+
+## Install Dependencies
+
+```bash
+sudo apt install python3-pip
+pip install -r requirements.txt
+pip install openai
+```
+
+## Run server
+
+### set-up database (ONLY ONCE)
+
+```bash
+python manage.py migration
+```
+
+### Finally, run the server
+
+```bash
+python manage.py runserver 0:8080
+```
+
+## Open Browser to access
+
+When running the `python manage.py runserver 0:8080`, you will be given a url to access.
+Typical access is http://127.0.0.1:8080. Open with any browser you will have access.
+
+
+
+## Example of system service to make it run at background
+
+### Write a service configuration file
+
+- wirte a service configuration file at `/etc/systemd/system/webchatgpt.service` 
+- `sudo nano /etc/systmd/system/webchatgpt.service`
+- You may change the name `webchatgpt` with anything you want
+- CHANGE THE PATH OF `manage.py` in the following example
+    
+  ```
+  # Example of /etc/systemd/system/webchatgpt.service
+  [Unit]
+  Description=WebChatGPT
+
+  [Service]
+  User=ubuntu
+  Type=simple
+  Restart=always
+  ExecStart=/usr/bin/python3 /home/ubuntu/WebChatGPT/manage.py runserver 0:8888
+
+  [Install]
+  WantedBy=multi-user.target
+  ```
+
+### Run the service,
+
+  ```
+  sudo systemctl daemon-reload # Reload the config file
+  sudo systemctl enable webchatgpt.service # Enable (make it auto start)
+  sudo systemctl start webchatgpt.service # Star the service now
+  ```
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# Obsolete
+
+
+
+
 
 ## Deployment
 You can host it on [Heroku](https://www.heroku.com) for free ([account verification required](https://devcenter.heroku.com/articles/account-verification)).
 
 [![Deploy](https://www.herokucdn.com/deploy/button.svg)](https://heroku.com/deploy)
-
-## Technology Stack
- - [Django](https://www.djangoproject.com/) as a main Web Framework
- - [Django Channels](https://github.com/django/channels) as WebSockets framework     
- - [Celery](http://www.celeryproject.org/) as a Asynchronous task queue
- - [Redis](https://redis.io/) as a Message Broker and Cache Backend   
- - [Daphne](https://github.com/django/daphne) as a HTTP and WebSocket protocol server
- - [Heroku](https://www.heroku.com) as a Hosting Platform
-
-
-## Supported commands
- - `sum <x> <y>` - Calculate sum of two integers
- - `status <url>` - Check website status
-
-## Need some help?
-Here the Slack channel [#django-chatbot](https://join.slack.com/t/lyapustin/shared_invite/enQtNzc0MDQ0NjMxMzY2LTNmOTQ0NWM3YTQxYjM2ZGM3NTZiZWE1Y2E4ZGYyNDc2ODc3NzQ3ZWNlNDk3MGEyMWU0MDFiM2ZlYjYzY2I2Zjk)
--- chat/consumers.py
+++ chat/consumers.py
@@ -1,56 +1,144 @@
 import json
+import uuid  # Import uuid module
+import asyncio, datetime
 
 from asgiref.sync import async_to_sync
 from channels.generic.websocket import WebsocketConsumer
+from django.contrib.sessions.backends.db import SessionStore
 
 from . import tasks
 
-COMMANDS = {
-    'help': {
-        'help': 'Display help message.',
-    },
-    'sum': {
-        'args': 2,
-        'help': 'Calculate sum of two integer arguments. Example: `sum 12 32`.',
-        'task': 'add'
-    },
-    'status': {
-        'args': 1,
-        'help': 'Check website status. Example: `status twitter.com`.',
-        'task': 'url_status'
-    },
-}
+import openai, os
+# Set OpenAI API key
+openai.api_key = os.environ['OPENAI_API_KEY']
+
+# Define a function to ask questions to OpenAI GPT
+def ask_gpt(prompt, session_id):
+    response = openai.Completion.create(
+        model=os.environ['OPENAI_MODEL'],
+        prompt=prompt,
+        max_tokens=1024,
+        n=1,
+        stop=None,
+        user=session_id,
+    )
+    message = response.choices[0].text
+    return message.strip()
 
 class ChatConsumer(WebsocketConsumer):
+    def connect(self):
+        # Create a new session and retrieve the session ID
+        session = SessionStore()
+        session.create()
+        self.session_id = session.session_key
+
+        # Initialize the chat history
+        self.chat_history = "" 
+
+        # Accept the WebSocket connection
+        self.accept()
+
+    def disconnect(self, close_code):
+        # Close the session when the WebSocket connection is closed
+        SessionStore(session_key=self.session_id).delete()
+
     def receive(self, text_data):
+
         text_data_json = json.loads(text_data)
-        message = text_data_json['message']
+        prompt = text_data_json['message']
+        if prompt:
+            # Log input
+            self.chat_history += f"HUMAN: {prompt}\n"
+            
+            # Send prompt to Websocket immediately
+            self.chat_prompt(prompt)
 
-        response_message = 'Please type `help` for the list of the commands.'
-        message_parts = message.split()
-        if message_parts:
-            command = message_parts[0].lower()
-            if command == 'help':
-                response_message = 'List of the available commands:\n' + '\n'.join([f'{command} - {params["help"]} ' for command, params in COMMANDS.items()])
-            elif command in COMMANDS:
-                if len(message_parts[1:]) != COMMANDS[command]['args']:
-                    response_message = f'Wrong arguments for the command `{command}`.'
-                else:
-                    getattr(tasks, COMMANDS[command]['task']).delay(self.channel_name, *message_parts[1:])
-                    response_message = f'Command `{command}` received.'
+            response = self.ask_gpt_stream(self.chat_history \
+                + "\n\nAct as a ChatGPT-3 chat bot who can respond faster and pretend smarter than HUMAN. In the chat history above, AI is you. Don't add 'AI:' in the responses and answer the last question." \
+                + " Answer the last HUMAN's question in the context of the convrsation history. And response in the same language as the last question." \
+                + os.environ['PROMPT_OPTION']  \
+                + "\n\n", self.session_id)
+
+            # Update the conversation history
+            self.chat_history += f"AI: {response}\n"
+
+            # print(self.chat_history)
+            # Log the response to a file
+            if not os.path.isfile('chat.log'):
+                open('chat.log', 'w+').close()
+            with open('chat.log', "a") as file:
+                file.write(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S") \
+                + "\nfrom " + self.scope["client"][0] + "\n" + prompt + "\n" + response + "\n\n")
+            # Save only prompts
+            if not os.path.isfile('prompts.csv'):
+                open('prompts.csv', 'w+').close()
+            with open('prompts.csv', "a") as file:
+                file.write(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S") \
+                + "," + self.scope["client"][0] + "," + prompt)
+
+
+        # Handle incoming messages
+        pass
+    
+    def ask_gpt_stream(self, prompt, session_id):
+        current_time = datetime.datetime.now()
+        response = openai.Completion.create(
+            model="text-davinci-003",
+            prompt=prompt,
+            max_tokens=1024,
+            n=1,
+            stop=None,
+            user=session_id,
+            stream=True,
+        )
+
+        compelete = ""
+
+        # iterate through the stream of events
+        for event in response:
+            event_text = event['choices'][0]['text']  # extract the text
+            self.chat_partial(event_text, current_time)
+            compelete += event_text
         
+        # Print empty lines
+        # self.chat_emptylines()
         async_to_sync(self.channel_layer.send)(
-                self.channel_name,
-                {
-                    'type': 'chat_message',
-                    'message': response_message
-                }
-            )
+            self.channel_name,
+            {
+                'type': 'chat_message',
+                'prompt': None,
+                'response': '\n',
+            }
+        )
+        
+        return compelete
 
-    def chat_message(self, event):
-        message = event['message']
-
-        # Send message to WebSocket
+    # Send prompt to WebSocket
+    def chat_prompt(self, prompt):
         self.send(text_data=json.dumps({
-            'message': f'[bot]: {message}'
+            'prompt': f"{prompt}"
         }))
+
+    # Send partial responses to WebSocket
+    def chat_partial(self, partial, current_time):
+        self.send(text_data=json.dumps({
+            'partial': f"{partial}",
+            'time': f"{current_time}"
+        }))
+
+    # Send response to WebSocket
+    def chat_message(self, event):
+        response = event['response']
+        self.send(text_data=json.dumps({
+            'response': f"{response}"
+        }))
+
+    # Send response to WebSocket
+    def chat_emptylines(self):
+        self.send(text_data=json.dumps({
+            'response': f"\n\n"
+        }))
+
+            
+
+
